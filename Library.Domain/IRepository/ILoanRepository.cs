3f62c43b280c8c437b6aa96aec9135507df96c6a
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-03 02:52:12 -0800

    write interface for Loan class

---
+++ Library.Domain/IRepository/ILoanRepository.cs
@@ -0,0 +1,17 @@
+﻿using Library.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.IRepository
+{
+    public interface ILoanRepository
+    {
+        void LoanBook(int bookId, int userId);
+        void ReturnBook(int bookId);
+
+        IEnumerable<Loan> GetActiveLoans();
+    }
+}
