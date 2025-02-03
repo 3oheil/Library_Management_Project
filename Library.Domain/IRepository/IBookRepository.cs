ace5926a713c9453c40b6ff3912bcd85af8d2f4a
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-03 02:48:59 -0800

    write interface for Book in IBookRepository class

---
+++ Library.Domain/IRepository/IBookRepository.cs
@@ -0,0 +1,22 @@
+﻿using Library.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.IRepository
+{
+    public interface IBookRepository
+    {
+        void AddBook(Book book);
+        void UpdateBook(Book book);
+        void DeleteBook(int id);
+        Book GetBookById(int id);
+
+
+        IEnumerable<Book> GetAllBooks();
+        IEnumerable<Book> SearchBooksByTitle(string title);
+        IEnumerable<Book> SearchBooksByAuther(string auther);
+    }
+}
