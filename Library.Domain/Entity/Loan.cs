cf8dfa8fb13065769e5ffe93804182948bed3e23
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-03 03:25:50 -0800

    change Loan properties

--- Library.Domain/Entity/Loan.cs
+++ Library.Domain/Entity/Loan.cs
@@ -8,8 +8,9 @@ namespace Library.Domain.Entity
 {
     public class Loan
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
