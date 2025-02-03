2b7676df50533fac1bd2b25c2ab82329e2b8bee0
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-03 03:20:10 -0800

    emplement IUserRepository in UserRepository

---
+++ Library.Data/Repository/UserRepository.cs
@@ -0,0 +1,56 @@
+﻿using Library.Domain.Entity;
+using Library.Domain.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Library.Data.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private List<User> users = new();
+
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
+        public void DeleteUser(int id)
+        {
+            var isexist = users.FirstOrDefault(b => b.Id == id);
+            if (isexist == null)
+            {
+                throw new NullReferenceException("No user was found with this id");
+            }
+
+            users.Remove(isexist);
+        }
+
+        public IEnumerable<User> GetAllUsers() => users;
+        
+
+        public User GetUser(int id)
+        {
+            return users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public IEnumerable<User> SearchUsersByName(string name)
+        {
+            return users.Where(u=>u.Name == name);
+        }
+
+        public void UpdateUser(User user)
+        {
+            var isExist = GetUser(user.Id);
+            if (isExist == null)
+            {
+                throw new NullReferenceException("No user was found with this id");
+            }
+            isExist.Email = user.Email;
+            isExist.Name = user.Name;
+        }
+    }
+}
