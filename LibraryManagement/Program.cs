0c9d64da8ea60436fbd2edb76c9752944d1b9074
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-03 04:27:40 -0800

    write code to Run in Program class

--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LibraryManagement
+﻿using Library.Data.Repository;
+using Library.Domain.IRepository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryManagement
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IBookRepository, BookRepository>()
+                .AddSingleton<IUserRepository, UserRepository>()
+                .AddSingleton<ILoanRepository, LoanRepository>()
+                .BuildServiceProvider();
+
+            LibraryTests.RunTests(serviceProvider);
         }
     }
 }
