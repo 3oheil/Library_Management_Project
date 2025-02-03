08578da958f5e3d382ec1c772cebafb68b97db45
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-03 04:13:02 -0800

    create a new project <Consoleapp> for test and Run project

---
+++ LibraryManageent/LibraryTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManageent
+{
+    public class LibraryTest
+    {
+        public static void RunTests(IServiceProvider serviceProvider)
+        {
+            var bookRepo = serviceProvider.GetService<IBookRepository>();
+            var userRepo = serviceProvider.GetService<IUserRepository>();
+            var loanRepo = serviceProvider.GetService<ILoanRepository>();
+        }
+    }
+}
