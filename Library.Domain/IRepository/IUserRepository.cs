937f0ff598feda137f679c2b693e6cbfdf554246
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-03 02:50:37 -0800

    write interface for User class

---
+++ Library.Domain/IRepository/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Library.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.IRepository
+{
+    public interface IUserRepository
+    {
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int id);
+        User GetUser(int id);
+
+
+        IEnumerable<User> GetAllUsers();
+        IEnumerable<User> SearchUsersByName(string name);
+    }
+}
